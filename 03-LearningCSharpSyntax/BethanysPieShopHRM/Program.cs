69fd0e07b25d619bc099c247a30e266e1e9e4a8a
kumailalidev <kumailalidev@gmail.com>
2024-07-25 03:46:13 +0500

    Using the Essential Building Blocks

---
+++ 03-LearningCSharpSyntax/BethanysPieShopHRM/Program.cs
@@ -0,0 +1,20 @@
+﻿// Single statement comment
+
+/*
+ * Multiline comments
+ * ....
+*/
+
+Console.WriteLine("Welcome to Bethany's Pie Shop HRM");
+
+Console.WriteLine("Please enter your name: ");
+
+// Valid identifiers
+
+string name = Console.ReadLine();
+string name2 = Console.ReadLine();
+string Name = Console.ReadLine(); // C# is case sensitive
+string Name_2 = Console.ReadLine();
+
+// Invalid identifiers
+// string 1Name = Console.ReadLine();
