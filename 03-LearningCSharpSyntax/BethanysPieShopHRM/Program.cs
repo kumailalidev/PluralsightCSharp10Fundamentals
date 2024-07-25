b8be10f53a84aa1a8bd8e32e3131e80473b6d35b
kumailalidev <kumailalidev@gmail.com>
2024-07-26 01:43:46 +0500

    Demo-Working with Primitive Types

--- 03-LearningCSharpSyntax/BethanysPieShopHRM/Program.cs
+++ 03-LearningCSharpSyntax/BethanysPieShopHRM/Program.cs
@@ -5,10 +5,10 @@ Console.WriteLine("Please enter your name: ");
 // Valid identifiers
 // ###########################################################################################
 
-string name = Console.ReadLine();
-string name2 = Console.ReadLine();
-string Name = Console.ReadLine(); // C# is case sensitive
-string Name_2 = Console.ReadLine();
+//string name = Console.ReadLine();
+//string name2 = Console.ReadLine();
+//string Name = Console.ReadLine(); // C# is case sensitive
+//string Name_2 = Console.ReadLine();
 
 // Invalid identifiers
 // ###########################################################################################
@@ -24,3 +24,22 @@ string Name_2 = Console.ReadLine();
  * Multiline comments
  * ....
 */
+
+// Primitive Types
+// ###########################################################################################
+
+// 32-bit signed integer
+int monthlyWage = 400;
+int month = 3, bonus = 300;
+
+// bool
+bool isActive = true;
+
+// byte 8-bit unsigned integer
+byte numberOfEmployees = 255;
+
+// double
+double rating = 4.93;
+
+// Type safety; Once a variable is declared its type can not be changed
+// isActive = "false";
