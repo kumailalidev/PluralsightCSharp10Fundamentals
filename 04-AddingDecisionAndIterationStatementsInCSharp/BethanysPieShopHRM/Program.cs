d3255b45457b0ff7eeac675e649f7c1d48cc0e4a
kumailalidev <kumailalidev@gmail.com>
2024-08-05 00:30:47 +0500

    Demo-Using if Statements

--- 04-AddingDecisionAndIterationStatementsInCSharp/BethanysPieShopHRM/Program.cs
+++ 04-AddingDecisionAndIterationStatementsInCSharp/BethanysPieShopHRM/Program.cs
@@ -22,6 +22,34 @@ Console.WriteLine("age1 is greater than 18 AND age2 is less than 65: " + d);
 bool e = (age1 >= 18) || (age2 <= 65);
 Console.WriteLine("age2 is greater than 18 OR age2 is less than 65: " + e);
 
+Console.WriteLine();
 Console.WriteLine("Using if Statements");
 Console.WriteLine("###########################################################################################");
 Console.WriteLine();
+
+// Example 1
+Console.WriteLine("Enter the age of the new candidate: ");
+int age4 = int.Parse(Console.ReadLine());
+
+if (age4 < 18)
+{
+    Console.WriteLine("Too young to apply");
+}
+else if (age4 > 65)
+{
+    Console.WriteLine("Sorry, the selected age is too old");
+}
+else
+{
+    Console.WriteLine("Great, you can now start with the application!");
+}
+
+// Example 2
+DateTime today = DateTime.Now;
+bool endOfMonthPaymentStarted = false;
+
+if (today.Date.Day == 20)
+    Console.WriteLine("Please start end-of-month payments");
+else if (today.Date.Day >= 25 && !endOfMonthPaymentStarted)
+    Console.WriteLine("Payments will be late");
+// else isn't required
