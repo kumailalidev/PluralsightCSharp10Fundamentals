23c0bec963c335e8280af7ee33e4c250b9a8c7f6
kumailalidev <kumailalidev@gmail.com>
2024-08-05 00:19:31 +0500

    Working with Relational Operators.

--- 04-AddingDecisionAndIterationStatementsInCSharp/BethanysPieShopHRM/Program.cs
+++ 04-AddingDecisionAndIterationStatementsInCSharp/BethanysPieShopHRM/Program.cs
@@ -1,1 +1,27 @@
-﻿Console.WriteLine("Welcome to Bethany's Pie Shop HRM");
+﻿Console.WriteLine("Working with Relational Operators");
+Console.WriteLine("###########################################################################################");
+Console.WriteLine();
+
+int age = 23;
+bool a = age == 23;
+Console.WriteLine("Age is 23: " + a);
+
+bool b = age > 23;
+Console.WriteLine("Age is greater than 23: " + b);
+
+bool c = (age >= 18) && (age <= 65);
+Console.WriteLine("Age is between 18 and 65: " + c);
+
+// Logical AND
+int age1 = 16;
+int age2 = 64;
+bool d = (age1 >= 18) && (age2 <= 65);
+Console.WriteLine("age1 is greater than 18 AND age2 is less than 65: " + d);
+
+// Logical OR
+bool e = (age1 >= 18) || (age2 <= 65);
+Console.WriteLine("age2 is greater than 18 OR age2 is less than 65: " + e);
+
+Console.WriteLine("Using if Statements");
+Console.WriteLine("###########################################################################################");
+Console.WriteLine();
