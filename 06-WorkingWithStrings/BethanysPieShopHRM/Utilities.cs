f6c0da4380b7bc255cb03946107bd5d83e4e0c14
kumailalidev <kumailalidev@gmail.com>
2024-08-13 06:51:13 +0500

    A Small Recap on Strings

---
+++ 06-WorkingWithStrings/BethanysPieShopHRM/Utilities.cs
@@ -0,0 +1,16 @@
+﻿namespace BethanysPieShopHRM
+{
+    internal class Utilities
+    {
+        public static void UsingSimpleStrings()
+        {
+            string firstName = "Bethany";
+            string lastName = "Smith";
+            string s;
+            s = firstName;
+            var userName = "BethanyS";
+            userName = userName.ToLower();
+            userName = ""; // Identical to string.Empty
+        }
+    }
+}
