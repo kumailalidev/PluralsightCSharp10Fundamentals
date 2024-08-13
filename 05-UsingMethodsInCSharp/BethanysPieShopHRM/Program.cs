9062f7738729286cb8a8b70332806f4c5da76225
kumailalidev <kumailalidev@gmail.com>
2024-08-13 06:02:17 +0500

    Adding a Helper File

--- 05-UsingMethodsInCSharp/BethanysPieShopHRM/Program.cs
+++ 05-UsingMethodsInCSharp/BethanysPieShopHRM/Program.cs
@@ -1,4 +1,6 @@
-﻿Console.WriteLine("05. Using Methods in C#");
+﻿using BethanysPieShopHRM;
+
+Console.WriteLine("05. Using Methods in C#");
 Console.WriteLine("###########################################################################################\n");
 
 Console.WriteLine("Creating and Using Methods");
@@ -7,19 +9,6 @@ Console.WriteLine("#############################################################
 int amount = 1234;
 int months = 12;
 
-int yearlyWage = CalculateYearlyWage(amount, months);
+int yearlyWage = Utilities.CalculateYearlyWage(amount, months);
 
 Console.WriteLine($"Yearly wage: {yearlyWage}");
-
-// Method that calculates early wage
-static int CalculateYearlyWage(int monthlyWage, int numberOfMonthsWorked)
-{
-    //Console.WriteLine($"Yearly wage: {monthlyWage * numberOfMonthsWorked}");
-    //return monthlyWage * numberOfMonthsWorked;
-
-    if (numberOfMonthsWorked == 12)
-        // Add a bonus month
-        return monthlyWage * (numberOfMonthsWorked + 1);
-
-    return monthlyWage * numberOfMonthsWorked;
-}
---
+++ 05-UsingMethodsInCSharp/BethanysPieShopHRM/Utilities.cs
@@ -0,0 +1,18 @@
+﻿namespace BethanysPieShopHRM
+{
+    internal class Utilities
+    {
+        // Method that calculates early wage
+        public static int CalculateYearlyWage(int monthlyWage, int numberOfMonthsWorked)
+        {
+            //Console.WriteLine($"Yearly wage: {monthlyWage * numberOfMonthsWorked}");
+            //return monthlyWage * numberOfMonthsWorked;
+
+            if (numberOfMonthsWorked == 12)
+                // Add a bonus month
+                return monthlyWage * (numberOfMonthsWorked + 1);
+
+            return monthlyWage * numberOfMonthsWorked;
+        }
+    }
+}
