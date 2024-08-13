98f82176ccb50c87707ff62436b1922e8d9b052c
kumailalidev <kumailalidev@gmail.com>
2024-08-13 06:45:24 +0500

    Exploring the Main Method

--- 05-UsingMethodsInCSharp/BethanysPieShopHRM/Program.cs
+++ 05-UsingMethodsInCSharp/BethanysPieShopHRM/Program.cs
@@ -1,32 +1,38 @@
 ﻿using BethanysPieShopHRM;
 
-Console.WriteLine("05. Using Methods in C#");
-Console.WriteLine("###########################################################################################\n");
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("05. Using Methods in C#");
+        Console.WriteLine("###########################################################################################\n");
 
-Console.WriteLine("Creating and Using Methods");
-Console.WriteLine("###########################################################################################\n");
+        Console.WriteLine("Creating and Using Methods");
+        Console.WriteLine("###########################################################################################\n");
 
-int amount = 1234;
-int months = 12;
+        int amount = 1234;
+        int months = 12;
 
-int yearlyWage = Utilities.CalculateYearlyWage(amount, months);
+        int yearlyWage = Utilities.CalculateYearlyWage(amount, months);
 
-Console.WriteLine($"Yearly wage: {yearlyWage}");
+        Console.WriteLine($"Yearly wage: {yearlyWage}");
 
-Console.WriteLine("\nUsing Method Overloading");
-Console.WriteLine("###########################################################################################\n");
+        Console.WriteLine("\nUsing Method Overloading");
+        Console.WriteLine("###########################################################################################\n");
 
-int bonus = 1000;
-Utilities.CalculateYearlyWage(amount, months, bonus);
+        int bonus = 1000;
+        Utilities.CalculateYearlyWage(amount, months, bonus);
 
-double amountDouble = 1500.0;
-double monthsDouble = 12;
-double bonusDouble = 1000;
+        double amountDouble = 1500.0;
+        double monthsDouble = 12;
+        double bonusDouble = 1000;
 
-double yearlyWageWithBonusDouble = Utilities.CalculateYearlyWage(amountDouble, monthsDouble, bonusDouble);
+        double yearlyWageWithBonusDouble = Utilities.CalculateYearlyWage(amountDouble, monthsDouble, bonusDouble);
 
-Console.WriteLine("\nUsing Optional Parameters and Named Arguments");
-Console.WriteLine("###########################################################################################\n");
+        Console.WriteLine("\nUsing Optional Parameters and Named Arguments");
+        Console.WriteLine("###########################################################################################\n");
 
-Utilities.UsingOptionalParameters();
-Utilities.UsingNamedArguments();
+        Utilities.UsingOptionalParameters();
+        Utilities.UsingNamedArguments();
+    }
+}
