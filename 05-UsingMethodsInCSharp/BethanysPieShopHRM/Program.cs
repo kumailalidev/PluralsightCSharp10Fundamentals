5b99761ea8d35f0a6771d6bcdae51c9d4ae15ac3
kumailalidev <kumailalidev@gmail.com>
2024-08-13 05:54:07 +0500

    Creating and using methods

--- 05-UsingMethodsInCSharp/BethanysPieShopHRM/Program.cs
+++ 05-UsingMethodsInCSharp/BethanysPieShopHRM/Program.cs
@@ -1,2 +1,16 @@
 ﻿Console.WriteLine("05. Using Methods in C#");
 Console.WriteLine("###########################################################################################\n");
+
+Console.WriteLine("\nCreating and Using Methods");
+Console.WriteLine("###########################################################################################\n");
+
+int amount = 1234;
+int months = 12;
+
+CalculateYearlyWage(amount, months);
+
+// Method that calculates early wage
+static void CalculateYearlyWage(int monthlyWage, int numberOfMonthsWorked)
+{
+    Console.WriteLine($"Yearly wage: {monthlyWage * numberOfMonthsWorked}");
+}
